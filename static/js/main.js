9c362622a9fb7bb22e02b77f69da16d68be3811b
Chaeyong park <pcy8201@postech.ac.kr>
2020-08-24 14:57:23 +0900

    scroll button

--- static/js/main.js
+++ static/js/main.js
@@ -8,3 +8,12 @@ $('.scroll-btn').click(function(e) {
     $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
     return false;
 });
+
+
+$(window).on("scroll", function(){    
+    if ( $( this ).scrollTop() > 200 ) {
+        $( '.scroll-btn' ).fadeIn();
+    } else {
+        $( '.scroll-btn' ).fadeOut();
+    }
+});
