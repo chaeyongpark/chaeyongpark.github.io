bc1987e2d51ad2df50f3ccced55a39dab11cb80f
Chaeyong park <pcy8201@postech.ac.kr>
2020-08-24 14:54:58 +0900

    scroll button

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	$('.sidenav').sidenav();
-
 	$('.research-card').matchHeight();
+	$('.fixed-action-btn').floatingActionButton();
+});
+
+$('.scroll-btn').click(function(e) {
+    $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+    return false;
 });
