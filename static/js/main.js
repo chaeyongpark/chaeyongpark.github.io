2b82e82bb182ad37d898a02f82409ee1ed73092b
Chaeyong park <pcy8201@postech.ac.kr>
2021-04-25 20:18:15 +0900

    add ToH

--- static/js/main.js
+++ static/js/main.js
@@ -161,3 +161,31 @@ $(function() {
     M.toast({html: 'COPY TO CLIPBOARD'});
   });
 });
+
+/*
+ * TOH 2021: Length Perception Model
+ */
+$(function() {
+  $("#TOH2021-Length-perception #citation-bibtex").click( function() {
+    if ($(this).text().includes("BIBTEX")) {
+      $("#TOH2021-Length-perception .description").html("@article{Park2021:Lengthperception,<br>&nbsp;&nbsp;author = {Park, Chaeyong and Kim, Jinsoo and Choi, Seungmoon},<br>&nbsp;&nbsp;journal={IEEE Transactions on Haptics},<br>&nbsp;&nbsp;title={Length Perception Model for Hand-held Controllers: The Effects of Diameter and Inertia},<br>&nbsp;&nbsp;year={2021},<br>&nbsp;&nbsp;volume={X},<br>&nbsp;&nbsp;number={X},<br>&nbsp;&nbsp;pages={XX-XX},<br>&nbsp;&nbsp;doi={}<br>}");
+      $(this).html("<i class=\"tiny material-icons left\">autorenew</i>CITATION");
+    } else {
+      $("#TOH2021-Length-perception .description").text("Chaeyong Park, Jinsoo Kim, and Seungmoon Choi. Length Perception Model for Hand-held Controllers: The Effects of Diameter and Inertia. IEEE Transactions on Haptics, Vol. X, no. X. 2021 (Accepted)");
+      $(this).html("<i class=\"tiny material-icons left\">autorenew</i>BIBTEX");
+    }
+  });
+});
+
+$(function() {
+  $("#TOH2021-Length-perception #citation-copy").click( function() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($("#TOH2021-Length-perception .description").text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    $(this).html("<i class=\"tiny material-icons left\">check</i>COPY TO CLIPBOARD");
+    M.toast({html: 'COPY TO CLIPBOARD'});
+  });
+});
